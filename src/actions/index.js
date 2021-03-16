be9f2cff0db6c612e05495643d161741f0fc7feb
tashiad <tashiadavis10@gmail.com>
2021-03-16 14:19:40 -0600

    Add filter actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,3 +7,15 @@ export const toggleToDo = (id) => ({
   type: 'TOGGLE_TODO',
   id
 });
+
+export const showAll = () => ({
+  type: 'SHOW_ALL'
+})
+
+export const showActive = () => ({
+  type: 'SHOW_ACTIVE'
+})
+
+export const showCompleted = () => ({
+  type: 'SHOW_COMPLETED'
+})
