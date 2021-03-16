970df7091ee09eef796394b92eb3738f4fd560ff
tashiad <tashiadavis10@gmail.com>
2021-03-16 13:57:48 -0600

    Render ToDos to the DOM

--- src/containers/ToDoList.js
+++ src/containers/ToDoList.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import Todo from '../components/ToDo';
 import { connect } from 'react-redux';
 
-const ToDoList = ({todos}) => {
-  console.log(todos);
+const ToDoList = ({ todos }) => {
+  const displayTodos = todos.map(todo => {
+    return (
+      <Todo
+        {...todo}
+        key={todo.id}
+      />
+    )
+  })
 
   return (
     <ul>
+      {displayTodos}
     </ul>
   )
 }
